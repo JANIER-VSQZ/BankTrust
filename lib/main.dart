06e29a039b218f077ced955739238db4f6d8fe6f
Rick282003 <richardgalo2003@gmail.com>
2025-07-14 17:35:53 -0600

    Fix color fondo

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class IniciarSesion extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           "Iniciar Sesión",
@@ -31,105 +32,107 @@ class IniciarSesion extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            const Align(
-              alignment: Alignment.center,
-              child: Text(
-                "Inicia sesión para continuar",
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-            const SizedBox(height: 80),
-
-            SizedBox(
-              width: 350,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.white,
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   const Text(
-                    "NÚMERO DE CUENTA",
-                    style: TextStyle(fontSize: 20),
+                    "Inicia sesión para continuar",
+                    style: TextStyle(fontSize: 18),
                   ),
-                  const SizedBox(height: 5),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: "Ingrese su número",
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Color(0xFFcce1c6),
-                    ),
-                  ),
-                  const SizedBox(height: 25),
+                  const SizedBox(height: 40),
 
-                  const Text("CONTRASEÑA", style: TextStyle(fontSize: 20)),
-                  const SizedBox(height: 5),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Ingrese su contraseña",
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Color(0xFFcce1c6),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-
-                  Center(
+                  SizedBox(
+                    width: 350,
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        TextButton(
-                          onPressed: () {
-
-                            // Falta
-                          },
-                          child: const Text(
-                            "¿Olvidó la Contraseña?",
-                            style: TextStyle(fontSize: 16, color: Colors.blue),
-                          ),
+                        const Text(
+                          "NÚMERO DE CUENTA",
+                          style: TextStyle(fontSize: 20),
                         ),
                         const SizedBox(height: 5),
-                        TextButton(
-                          onPressed: () {
-                            // Falta
+                        const TextField(
+                          decoration: InputDecoration(
+                            labelText: "Ingrese su número",
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(0xFFcce1c6),
+                          ),
+                        ),
+                        const SizedBox(height: 25),
 
-                          },
-                          child: const Text(
-                            "Crear Cuenta",
-                            style: TextStyle(fontSize: 16, color: Colors.blue),
+                        const Text("CONTRASEÑA", style: TextStyle(fontSize: 20)),
+                        const SizedBox(height: 5),
+                        const TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Ingrese su contraseña",
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(0xFFcce1c6),
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+
+                        Center(
+                          child: Column(
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  // Recuperar contraseña olvidada
+                                },
+                                child: const Text(
+                                  "¿Olvidó la Contraseña?",
+                                  style: TextStyle(fontSize: 16, color: Colors.blue),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              TextButton(
+                                onPressed: () {
+                                  // Crear nueva cuenta
+                                },
+                                child: const Text(
+                                  "Crear Cuenta",
+                                  style: TextStyle(fontSize: 16, color: Colors.blue),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 50),
+
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Acción de login
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFF27662A),
+                              foregroundColor: Colors.white,
+                              textStyle: const TextStyle(fontSize: 20),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 15,
+                              ),
+                            ),
+                            child: const Text("Iniciar Sesión"),
                           ),
                         ),
                       ],
                     ),
                   ),
-                  const SizedBox(height: 50),
-
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Acción de login
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFF27662A),
-                        foregroundColor: Colors.white,
-                        textStyle: const TextStyle(fontSize: 20),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 15,
-                        ),
-                      ),
-                      child: const Text("Iniciar Sesión"),
-                    ),
-                  ),
+                  const SizedBox(height: 20),
                 ],
               ),
             ),
-
-            const SizedBox(height: 20),
-          ],
+          ),
         ),
       ),
     );
