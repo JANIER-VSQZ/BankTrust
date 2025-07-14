97a9a3a1548b946834d2c57fd38e684fb3a5d07d
Rick282003 <richardgalo2003@gmail.com>
2025-07-14 17:25:32 -0600

    Pagina de Iniciar Sesion

--- lib/main.dart
+++ lib/main.dart
@@ -29,68 +29,106 @@ class IniciarSesion extends StatelessWidget {
         ),
         centerTitle: true,
         elevation: 0,
+        backgroundColor: Colors.white,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25.0),
         child: Column(
           children: [
-            const SizedBox(height: 20), // Separación debajo del título
+            const SizedBox(height: 10),
             const Align(
-              alignment: Alignment.centerLeft,
+              alignment: Alignment.center,
               child: Text(
                 "Inicia sesión para continuar",
                 style: TextStyle(fontSize: 18),
               ),
             ),
-            const SizedBox(height: 30),
-
-            // Número de cuenta
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("NÚMERO DE CUENTA", style: TextStyle(fontSize: 20)),
-            ),
-            const SizedBox(height: 5),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Ingrese su número",
-                border: OutlineInputBorder(),
-                filled: true, // <- activa el fondo
-                fillColor: Color(
-                  0xFFcce1c6,
-                ),
-              ),
-            ),
-            const SizedBox(height: 25),
-
-            // Contraseña
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("CONTRASEÑA", style: TextStyle(fontSize: 20)),
-            ),
-            const SizedBox(height: 5),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Ingrese su contrasema",
-                border: OutlineInputBorder(),
-                filled: true,
-                fillColor: Color(
-                  0xFFcce1c6,
-                ),
-              ),
-            ),
-
-            const Spacer(), // para mandar el botón hacia abajo
+            const SizedBox(height: 80),
 
             SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Falta la de inisicar sesion
-                },
-                child: const Text("Ingresar y ver el Icono de Brayan la Cabra"),
+              width: 350,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "NÚMERO DE CUENTA",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(height: 5),
+                  const TextField(
+                    decoration: InputDecoration(
+                      labelText: "Ingrese su número",
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Color(0xFFcce1c6),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+
+                  const Text("CONTRASEÑA", style: TextStyle(fontSize: 20)),
+                  const SizedBox(height: 5),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Ingrese su contraseña",
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Color(0xFFcce1c6),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: () {
+
+                            // Falta
+                          },
+                          child: const Text(
+                            "¿Olvidó la Contraseña?",
+                            style: TextStyle(fontSize: 16, color: Colors.blue),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        TextButton(
+                          onPressed: () {
+                            // Falta
+
+                          },
+                          child: const Text(
+                            "Crear Cuenta",
+                            style: TextStyle(fontSize: 16, color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Acción de login
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF27662A),
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(fontSize: 20),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 15,
+                        ),
+                      ),
+                      child: const Text("Iniciar Sesión"),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20), // espacio de abajo
+
+            const SizedBox(height: 20),
           ],
         ),
       ),
