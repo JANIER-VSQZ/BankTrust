3506d9489bea1be302c16915e1c3481f8c36195c
DiegoHernandez987 <dihernandez987@unicah.edu>
2025-07-20 23:20:45 -0600

    correccionesdiego

--- lib/screen/pagarservicios.dart
+++ lib/screen/pagarservicios.dart
@@ -23,165 +23,173 @@ class _PagarserviciosState extends State<Pagarservicios> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            color: const Color(0xFF328535),
-            child: Center(
-              child: Text(
-                'PAGO DE SERVICIOS',
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontSize: 29.3,
-                  fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              color: const Color(0xFF328535),
+              child: Center(
+                child: Text(
+                  'PAGO DE SERVICIOS',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 29.3,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Datos',
-            style: GoogleFonts.poppins (fontSize: 45, color: Color(0xFF328535)),
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'CUENTA DE ORIGEN:',
-                style: GoogleFonts.dmSans(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black54,
+            const SizedBox(height: 20),
+            Text(
+              'Datos',
+              style: GoogleFonts.poppins(
+                fontSize: 45,
+                color: Color(0xFF328535),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'CUENTA DE ORIGEN:',
+                  style: GoogleFonts.dmSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 5),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: TextField(
-              controller: cuentaOrigen,
-              decoration: const InputDecoration(
-                enabled: false,
-                labelText: "Cuenta de origen",
-                border: OutlineInputBorder(),
-                filled: true,
-                fillColor: Color.fromARGB(255, 222, 222, 222),
+            const SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: TextField(
+                controller: cuentaOrigen,
+                decoration: const InputDecoration(
+                  enabled: false,
+                  labelText: "123",
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 222, 222, 222),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 30),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'SERVICIO:',
-                      style: GoogleFonts.dmSans(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black54,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'SERVICIO:',
+                        style: GoogleFonts.dmSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 12),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    children: [
-                      DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFcce1c6),
-                          borderRadius: BorderRadius.circular(6),
+                  const SizedBox(height: 12),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                    child: Column(
+                      children: [
+                        DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFcce1c6),
+                            borderRadius: BorderRadius.circular(6),
+                            border: Border.all(color: Colors.black, width: 1),
+                          ),
+                          child: DropdownButton<String>(
+                            isExpanded: true,
+                            hint: Text("Elija una opcion"),
+                            iconEnabledColor: Colors.white,
+                            underline: const SizedBox(),
+                            dropdownColor: const Color(0xFFcce1c6),
+                            value: _seleccion,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                            ),
+                            items: _opciones.map((String valor) {
+                              return DropdownMenuItem<String>(
+                                value: valor,
+                                child: Text(valor),
+                              );
+                            }).toList(),
+                            onChanged: (String? nuevoValor) {
+                              setState(() {
+                                _seleccion = nuevoValor;
+                              });
+                            },
+                          ),
                         ),
-                        child: DropdownButton<String>(
-                        isExpanded: true,
-                        hint: const Text(
-                          'Elige una opción',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        iconEnabledColor: Colors.white,
-                        underline: const SizedBox(),
-                        dropdownColor: Colors.white,
-                        value: _seleccion,
-                        items: _opciones.map((String valor) {
-                          return DropdownMenuItem<String>(
-                            value: valor,
-                            child: Text(valor),
-                          );
-                        }).toList(),
-                        onChanged: (String? nuevoValor) {
-                          setState(() {
-                            _seleccion = nuevoValor;
-                          });
-                        },
-                      ),
-                      ),
-                      
-                    ],
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'CANTIDAD A PAGAR:',
+                  style: GoogleFonts.dmSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'CANTIDAD A PAGAR:',
-                style: GoogleFonts.dmSans(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black54,
+            const SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: TextField(
+                controller: cantpagar,
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: const InputDecoration(
+                  enabled: true,
+                  labelText: "L.0.00",
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Color(0xFFcce1c6),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 5),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: TextField(
-              controller: cantpagar,
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-              decoration: const InputDecoration(
-                enabled: true,
-                labelText: "L.0.00",
-                border: OutlineInputBorder(),
-                filled: true,
-                fillColor: Color(0xFFcce1c6),
-              ),
-            ),
-          ),
-          const SizedBox(height: 30),
-          Center(
-            child: ElevatedButton(
-              onPressed: mtdTransferencia,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF27662A),
-                foregroundColor: Colors.white,
-                textStyle: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w700),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 15,
+            const SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                onPressed: mtdTransferencia,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF27662A),
+                  foregroundColor: Colors.white,
+                  textStyle: GoogleFonts.poppins(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 15,
+                  ),
                 ),
+                child: const Text("Confirmar Pago"),
               ),
-              child: const Text("Confirmar Pago"),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
