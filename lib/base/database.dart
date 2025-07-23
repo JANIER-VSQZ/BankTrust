51a802758aca1ed10677ea0949d5f56321dcaa8e
JuanRa <jraguilar012012@gmail.com>
2025-07-23 17:29:09 -0600

    Base

---
+++ lib/base/database.dart
@@ -0,0 +1,76 @@
+// lib/base/database_helper.dart
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, 'my_database.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE CUENTAS(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            NUMERO INTEGER,
+            CLAVE VARCHAR(50),
+            NOMBRE VARCHAR(50)
+          )
+        ''');
+        await db.execute('''
+          CREATE TABLE TRANSFERENCIAS(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            ID_CUENTA INTEGER REFERENCES CUENTAS(ID),
+            ID_CUENTA_DESTINO INTEGER REFERENCES CUENTAS(ID),
+            FECHA DATE,
+            MONTO NUMERIC(12,2),
+            CONCEPTO VARCHAR(100)
+          )
+        ''');
+        await db.execute('''
+          CREATE TABLE PAGOS_TIPOS(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            DESCRIPCION VARCHAR(100)
+          )
+        ''');
+        await db.execute('''
+          CREATE TABLE PAGOS(
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            ID_CUENTA INTEGER REFERENCES CUENTAS(ID),
+            ID_TIPO INTEGER REFERENCES PAGOS_TIPOS(ID),
+            FECHA DATE,
+            MONTO NUMERIC(12,2)
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> getAllCuentas() async {
+    final db = await database;
+    return await db.query('CUENTAS');
+  }
+
+  Future<void> insertCuenta(String nombre, int numero, String clave) async {
+    final db = await database;
+    await db.insert('CUENTAS', {
+      'NOMBRE': nombre,
+      'NUMERO': numero,
+      'CLAVE': clave,
+    }, conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+}
